4862b0c11cee9c30cd2ec0d473853656fb0d5fe9
James Maranga <jmaranga35@gmail.com>
2024-07-04 15:19:05 +0300

    second update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,12 @@
 import Headerbox from '@/components/Headerbox'
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
   const loggedIn ={
-    firstName: 'James'
+    firstName: 'James', lastName: 'Kamanda',
+    email: 'jmaranga@gmail.com'
   };
   return (
     <section className='Home'>
@@ -15,14 +17,27 @@ const Home = () => {
           title="Welcome to MKOBA"
           user={loggedIn?.firstName || 'Guest'}
           subtext='Access and manage your account and transactions first and effeciently.'
+          
           />
           <TotalBalanceBox
           accounts={[]}
           totalbanks={1}
-          totalCurrentBalance={1500.0}
+          totalCurrentBalance={1500000.0}
           />
         </header>
+
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar 
+      user={loggedIn}
+      transactions={[]}
+
+      banks={[
+        { currentBalance: 17000.50},
+         {currentBalance: 67000.50}
+        ]}
+      />
+
     </section>
   )
 }
--- components/Headerbox.tsx
+++ components/Headerbox.tsx
@@ -8,19 +8,13 @@ const Headerbox = ({
     <div className="Headerbox">
       <h1 className="header-box-title ">
         {title}
-        {type === "greeting" &&
+        {type === "greeting" &&(
          <span className="text-bankGradient">
           &nbsp;{user}
-          </span>}
+          </span>
+          )}
       </h1>
-      <p className="header-box-subtext">{subtext}</p>
-
-      {type === "greeting" && (
-        <h1 className="text-2xl font-semibold text-gray-900 mb-2">
-          Welcome, {user}!
-        </h1>
-      )}
-      
+      <p className="header-box-subtext">{subtext}</p>    
     </div>
   );
 };
